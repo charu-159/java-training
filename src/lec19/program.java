e73f46043415367f337661230cab79ed17c5b4b6
charu-159 <charupatel159@gmail.com>
2025-12-12 22:11:29 +0530

    .

---
+++ src/lec19/program.java
@@ -0,0 +1,14 @@
+package lec19;
+
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("hello");
+        System.out.println("byeeee");
+        System.out.println("hiiii");
+        int n=sc.nextInt();
+
+            }
+        }
