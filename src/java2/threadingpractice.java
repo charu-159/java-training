a788072ca51a91e0b1c268e9752d41ae61d46b4e
charu-159 <charupatel159@gmail.com>
2025-12-15 21:56:12 +0530

    .

---
+++ src/java2/threadingpractice.java
@@ -0,0 +1,8 @@
+//good morning  and welcome continusoly on the screen in java
+//java using threads
+package java2;
+public class threadingpractice {
+    public static void main(String[] args) {
+
+    }
+}
