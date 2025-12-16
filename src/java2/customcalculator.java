a5195a24b909287f5e55fd33ba767c29b68925b9
charu-159 <charupatel159@gmail.com>
2025-12-16 22:53:49 +0530

    finallyblock

---
+++ src/java2/customcalculator.java
@@ -0,0 +1,18 @@
+package java2;
+public class customcalculator {
+    public static void main(String[] args) {
+        /*
+        exercise=you have to create a custom calculator which following operation
+        1. + ->addition
+        2. - -> substraction
+        3.*->mul
+        4./->division
+        which throws the following exception
+        1.invalid input exception eg=8&9
+        2.cannot / by 0 ex
+        3,max i/p exception if any of the input is grater than 10000
+        4.max multiply reached excep=dont allow any muktipl input is greater than 7000
+
+                 */
+    }
+}
---
+++ src/java2/errorandexceptionpractiseset.java
@@ -0,0 +1,6 @@
+package  java2;
+public class errorandexceptionpractiseset {
+    public static void main(String[] args) {
+       // question 1=
+    }
+}
