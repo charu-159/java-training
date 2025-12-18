4ac3dfa13b18dc7f51868807a112dbed224e39a2
charu-159 <charupatel159@gmail.com>
2025-12-18 11:55:44 +0530

    .

---
+++ src/lec21/jump.java
@@ -0,0 +1,23 @@
+package lec21;
+
+public class jump {
+
+    public static boolean canJump(int[] nums) {
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false; // agar hum is index tak pahunch hi nahi paaye
+            }
+            max = Math.max(max, i + nums[i]); // max reachable index update
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 0, 3};
+        System.out.println(canJump(nums));
+    }
+}
+
