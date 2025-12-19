a307c7c212dbea7889a2f82a55d6877b267d99d6
charu-159 <charupatel159@gmail.com>
2025-12-20 00:46:31 +0530

    .

---
+++ src/java2/dateclass.java
@@ -0,0 +1,16 @@
+package java2;
+
+import java.util.Date;
+
+public class dateclass {
+    public static void main(String[] args) {
+    Date d=new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        System.out.println(d.getDate());
+        System.out.println(d.getYear());
+
+
+
+    }
+}
