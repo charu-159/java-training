64ecd41bde340fdc0d14c226e2b5f045a41928a0
charu-159 <charupatel159@gmail.com>
2025-12-21 21:17:44 +0530

    .

---
+++ src/java2/dateandtime.java
@@ -0,0 +1,8 @@
+package java2;
+public class dateandtime {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);//divide 1000-sec 3600=min 24=hr 368=days
+         System.out.println(Long.MAX_VALUE);
+//        System.out.println(System.currentTimeMillis());
+    }
+}
