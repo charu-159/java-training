79f9e52ae43ea90a92761309673b7f65c4483a00
charu-159 <charupatel159@gmail.com>
2025-12-23 20:59:09 +0530

    .

---
+++ src/leetcode/code33.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+public class code33 {
+
+    // Binary search in rotated sorted array
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Left half sorted
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right half sorted
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        code33 obj = new code33();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        int result = obj.search(nums, target);
+        System.out.println("Index of target: " + result);
+    }
+}
