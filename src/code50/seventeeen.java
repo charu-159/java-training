03e91f12a901e993f0801b14c23e8e8f43628003
charu-159 <charupatel159@gmail.com>
2025-12-25 22:03:09 +0530

    ..

---
+++ src/code50/seventeeen.java
@@ -0,0 +1,15 @@
+//Find intersection of two sorted arrays
+package code50;
+public class seventeeen {
+    public static void main(String[] args) {
+        int[] arr1={1,2,3,4,5,6};
+        int [] arr2={5,6,7,8,9};
+        for(int i=0;i< arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    System.out.println("intersection"+arr1[i]);
+                }
+            }
+        }
+    }
+}
