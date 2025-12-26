38448a9c2792695625571a1892c4f06a89ffcf61
charu-159 <charupatel159@gmail.com>
2025-12-26 22:15:51 +0530

    ..

--- src/code50/eighteen.java
+++ src/code50/eighteen.java
@@ -1,8 +1,35 @@
 //trapping rain water
 package code50;
 public class eighteen {
-    public static void main(String[] args) {
-        int [] arr={4,2,0,3,2,5};
+        public static void main(String[] args) {
+            int[] height = {4, 2, 0, 3, 2, 5};
+            int water = trap(height);
+            System.out.println("Total trapped water = " + water);
+        }
 
+        public static int trap(int[] height) {
+            int left = 0, right = height.length - 1;
+            int leftMax = 0, rightMax = 0;
+            int water = 0;
+
+            while (left < right) {
+                if (height[left] < height[right]) {
+                    if (height[left] >= leftMax) {
+                        leftMax = height[left];
+                    } else {
+                        water += leftMax - height[left];
+                    }
+                    left++;
+                } else {
+                    if (height[right] >= rightMax) {
+                        rightMax = height[right];
+                    } else {
+                        water += rightMax - height[right];
+                    }
+                    right--;
+                }
+            }
+            return water;
+        }
     }
-}
+
