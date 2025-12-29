137022c955d778fb0bc28a3664fe7a8dafdea4ed
charu-159 <charupatel159@gmail.com>
2025-12-29 23:25:35 +0530

    20

---
+++ src/code50/twenty.java
@@ -0,0 +1,14 @@
+//maximum product subarray
+package code50;
+public class twenty{
+    public static int maxProduct(int[] nums) {
+        int n = nums.length;
+        int maxProduct = nums[0];
+
+
+
+//    public static void main(String[] args) {
+//        int[] arr = {-2, 6, -3, -10, 0, 2};
+//        System.out.println("Maximum Product Subarray = " + maxProduct(arr));
+//    }
+//}
