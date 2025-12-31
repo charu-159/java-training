8db2a125059ab2d7861b64f0a7d70e3fa18f4a24
charu-159 <charupatel159@gmail.com>
2025-12-31 15:43:02 +0530

    leeetcode

---
+++ src/leetcode/code283.java
@@ -0,0 +1,25 @@
+//moves zeroes
+package leetcode;
+public  class code283 {
+    public static void movezero(int []nums) {
+      int index=0;
+      for(int i=0;i<nums.length;i++){
+          if(nums[i]!=0){
+              nums[index]=nums[i];
+              index++;
+          }
+      }
+      while(index<nums.length){
+          nums[index]=0;
+          index++;
+      }
+    }
+    public static void main(String[] args) {
+        int [] arr={1,0,2,0,7,0};
+        movezero(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
