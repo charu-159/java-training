e1ee5fc9d173f634d07fd13e0da4c2458420baf6
charu-159 <charupatel159@gmail.com>
2026-01-01 23:27:26 +0530

    .

---
+++ src/leetcode/maxconsecutiveones.java
@@ -0,0 +1,26 @@
+package leetcode;
+public class maxconsecutiveones {
+    public   int consecutiveones(int[] nums) {
+        int maxcount = 0;
+        int currentcount = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                currentcount++;
+                maxcount = Math.max(maxcount, currentcount);
+            } else {
+                currentcount = 0;
+            }
+        }
+            return maxcount;
+
+
+    }
+
+    public static void main(String[] args) {
+          maxconsecutiveones obj=new maxconsecutiveones();
+          int [] nums={1,1,0,1,1,1,1};
+        System.out.println(obj.consecutiveones(nums));
+    }
+}
+
+
