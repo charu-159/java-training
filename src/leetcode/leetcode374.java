1918b3e20ccee4b805cd4bcdf036e677e2808a8d
charu-159 <charupatel159@gmail.com>
2026-01-01 23:27:09 +0530

    .

---
+++ src/leetcode/leetcode374.java
@@ -0,0 +1,10 @@
+//guess no.
+package leetcode;
+public class leetcode374 {
+    public static  int guessno(int nums){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("enter the value");
+    }
+}
