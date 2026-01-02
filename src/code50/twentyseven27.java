53dcbea27ec21b9d5f45f4e6a1e67b6448b9b03a
charu-159 <charupatel159@gmail.com>
2026-01-02 22:16:57 +0530

    .

---
+++ src/code50/twentyseven27.java
@@ -0,0 +1,19 @@
+//longest common prefix
+package code50;
+
+public class twentyseven27 {
+    public String commonprefix(String [] str){
+        if(str==null||str.length==0)
+            return "";
+            String prefix=str[0];
+            for(int i=1;i< str.length;i++){
+                while(str[i].indexOf(prefix)!=0){
+                    prefix=prefix.substring(0,prefix.length()-1);
+                    if(prefix.isEmpty())
+                        return "";
+                }
+            }
+            return prefix;
+
+        }
+    }
