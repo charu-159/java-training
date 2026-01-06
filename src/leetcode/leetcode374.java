a1e79360337cbe93b3c60dee1b9d51dbf03f6b69
charu-159 <charupatel159@gmail.com>
2026-01-06 09:12:39 +0530

    .

--- src/leetcode/leetcode374.java
+++ src/leetcode/leetcode374.java
@@ -1,10 +1,19 @@
 //guess no.
 package leetcode;
-public class leetcode374 {
-    public static  int guessno(int nums){
 
-    }
-    public static void main(String[] args) {
-        System.out.println("enter the value");
+import java.util.Map;
+import java.util.HashMap;
+
+public class leetcode374{
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {}; // return empty array if no solution
     }
 }
