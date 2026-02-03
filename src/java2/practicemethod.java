d11891b52aa78d73dfae77006759c136d2d11471
charu-159 <charupatel159@gmail.com>
2026-02-03 20:59:41 +0530

    .

--- src/java2/practicemethod.java
+++ src/java2/practicemethod.java
@@ -1,7 +1,22 @@
 package java2;
 
 public class practicemethod {
-    public static void main(String[] args) {
+    // Method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
+    // Method to print a greeting
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        // Calling add method
+        int sum = add(5, 10);
+        System.out.println("Sum is: " + sum);
+
+        // Calling greet method
+        greet("Charu");
     }
 }
