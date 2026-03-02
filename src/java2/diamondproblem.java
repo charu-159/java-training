cfc5434e41f76620943cc498e508c0df691f6f28
charu-159 <charupatel159@gmail.com>
2026-03-02 15:19:52 +0530

    diamond problem by virtual inheritance

---
+++ src/java2/diamondproblem.java
@@ -0,0 +1,40 @@
+// Base interface
+interface A {
+    void show();
+}
+
+// Interface B extends A
+interface B extends A {
+    default void show() {
+        System.out.println("Show from B");
+    }
+}
+
+// Interface C extends A
+interface C extends A {
+    default void show() {
+        System.out.println("Show from C");
+    }
+}
+
+// Class D implements both B and C
+class D implements B, C {
+    // Must override show() to resolve ambiguity
+    @Override
+    public void show() {
+        // Explicitly choose which interface's method to use
+        B.super.show();   // You can also use C.super.show()
+        System.out.println("Show from D");
+    }
+}
+
+public class diamondproblem {
+    public static void main(String[] args) {
+        D obj = new D();
+        obj.show();
+    }
+}
+
+;
+
+
