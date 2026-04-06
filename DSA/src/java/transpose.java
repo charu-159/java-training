ece07ac38b4d06f3b8e3c27d490b514fb8a3af3d
charu-159 <charupatel159@gmail.com>
2026-04-06 12:57:09 +0530

    dsa

--- DSA/src/java/transpose.java
+++ DSA/src/java/transpose.java
@@ -15,8 +15,8 @@ public class transpose {
 
         // Input matrix
         System.out.println("Enter matrix elements:");
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
@@ -24,20 +24,69 @@ public class transpose {
         // Transpose matrix
         int[][] transpose = new int[cols][rows];
 
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 transpose[j][i] = matrix[i][j];
             }
         }
 
         // Print transpose
         System.out.println("Transpose Matrix:");
-        for(int i = 0; i < cols; i++) {
-            for(int j = 0; j < rows; j++) {
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
                 System.out.print(transpose[i][j] + " ");
             }
             System.out.println();
         }
+        //found value
+        int d = 5;
+        boolean flag = true;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == d)
+                    System.out.println("found");
+
+                flag = false;
+            }
+
+        }
+
+        if (!flag) {
+            System.out.println("not found");
+        }
+
+
+        //spiral
+        int top = 0, bottom = rows - 1;
+        int left = 0, right = cols - 1;
+        System.out.println("spiral");
+        if (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                System.out.println(matrix[top][i] + " ");
+            }
+            top++;
+        for (int j = top; j <= bottom; j++) {
+            System.out.println(matrix[j][right]+" ");
+        }
+        right --;
+
+        for(int i=right;i>=left;i--){
+            System.out.println(matrix[bottom][i]+" ");
+        }
+        bottom--;
+        for(int i=bottom;i>=top;i--){
+            System.out.println(matrix[i][left]+" ");
+        }
+        left++;
+
+
+        }
+        //wave from spiral
+        int top = 0, bottom = rows - 1;
+        int left = 0, right = cols - 1;
+
+
+
 
         sc.close();
     }
