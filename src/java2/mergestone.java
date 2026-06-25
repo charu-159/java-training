bc8ed1c82d3965c8a6a696a198cfa117d52025c9
charu-159 <charupatel159@gmail.com>
2026-06-25 22:21:20 +0530

    dsa

---
+++ src/java2/mergestone.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mergeStones(int[] stones, int k) {
+        int n = stones.length;
+        if ((n - 1) % (k - 1) != 0) return -1; // impossible case
+
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) prefix[i + 1] = prefix[i] + stones[i];
+
+        int[][] dp = new int[n][n];
+        for (int len = k; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int m = i; m < j; m += (k - 1)) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][m] + dp[m + 1][j]);
+                }
+                if ((len - 1) % (k - 1) == 0) {
+                    dp[i][j] += prefix[j + 1] - prefix[i];
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
