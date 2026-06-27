c85fea0c2a9a867554c6c114d57685186f5dfe88
charu-159 <charupatel159@gmail.com>
2026-06-27 14:05:33 +0530

    dsa

---
+++ src/java2/subarraysum.java
@@ -0,0 +1,22 @@
+package java2;
+public class subarraysum {
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int k = 3;
+        System.out.println("Total subarrays with sum " + k + " = " + subarraySum(nums, k));
+    }
+}
